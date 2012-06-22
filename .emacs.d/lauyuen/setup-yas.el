8d66a187eabd961e1d2c5b793d55c50bfa34c891
Yuen Lau <me@yuen-lau.com>
2012-06-22 14:30:37 -0400

    .

---
+++ .emacs.d/lauyuen/setup-yas.el
@@ -0,0 +1,33 @@
+(message "loading yasnippet customizations ...")
+(yld-add-path "plugins/yasnippet")
+(require 'yasnippet)
+
+;; Use only own snippets, do not use bundled ones
+;; (setq yas/snippet-dirs '("~/.emacs.d/snippets"))
+(yas/global-mode 1)
+
+;; Jump to end of snippet definition
+(define-key yas/keymap (kbd "<return>") 'yas/exit-all-snippets)
+
+;; Inter-field navigation
+(defun yas/goto-end-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+        (position (yas/field-end (yas/snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-end-of-line-or-next-line)
+      (goto-char position))))
+
+(defun yas/goto-start-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+        (position (yas/field-start (yas/snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-start-of-line-or-prev-line)
+      (goto-char position))))
+
+(define-key yas/keymap (kbd "C-e") 'yas/goto-end-of-active-field)
+(define-key yas/keymap (kbd "C-a") 'yas/goto-start-of-active-field)
+
+;; No dropdowns please, yas
+(setq yas/prompt-functions '(yas/ido-prompt yas/completing-prompt))
