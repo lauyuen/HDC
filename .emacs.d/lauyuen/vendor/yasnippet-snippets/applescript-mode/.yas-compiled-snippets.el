202726c5b441082f1107f6854289bc6e434b5e1c
lauyuen <me@yuen-lau.com>
2012-07-27 12:03:18 -0400

    .

--- .emacs.d/lauyuen/vendor/yasnippet-snippets/applescript-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `applescript-mode'
-;;; Do not edit! File generated at Tue Jul 24 11:38:46 2012
--- .emacs.d/lauyuen/vendor/yasnippet-snippets/ledger-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `ledger-mode'
-;;; Do not edit! File generated at Tue Jul 24 11:38:49 2012
